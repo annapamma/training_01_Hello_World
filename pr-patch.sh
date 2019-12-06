389b3a3de094016339a84c626cfdbfb002da9566
annapamma <anna.pamela@gmail.com>
2019-12-05 21:25:33 -0500

    2

--- pr-patch.sh
+++ pr-patch.sh
@@ -1,3 +1,3 @@
-curl -H 'Authorization: token AFIC2KTDGJ77DPXOOPEHSFC533J5E' \
-     -H 'Accept: application/vnd.github.VERSION.patch' \
-     https://api.github.com/repos/annapamma/training_01_Hello_World/pulls/18
+curl -H "Authorization: token f9da63f319946885373a664970e7e40b41445064" \
+     -H "Accept: application/vnd.github.VERSION.patch" \
+     https://api.github.com/repos/annapamma/training_01_Hello_World/pulls/2
